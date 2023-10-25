f68d25bba091573b52f2bd046d3d018085d4a93f
BakhtZiaKhan <144609134+BakhtZiaKhan@users.noreply.github.com>
2023-10-25 13:31:37 +0100

    Update index.js
    fix

--- src/index.js
+++ src/index.js
@@ -31,29 +31,6 @@ process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
 process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon 
 
-const quotes = [
-  "Quote 1: Your first quote here.",
-  "Quote 2: Another inspiring quote.",
-  "Quote 3: Keep changing your quotes!",
-  "Quote 4: This is the fourth quote.",
-  "Quote 5: The final quote for now.",
-];
-
-document.addEventListener("DOMContentLoaded", function () {
-  const quoteContainer = document.getElementById("quote-container");
-  const quoteElement = document.getElementById("quote");
-
-  let currentQuoteIndex = 0;
-
-  function updateQuote() {
-    quoteElement.textContent = quotes[currentQuoteIndex];
-    currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
-  }
-
-  updateQuote();
-
-  setInterval(updateQuote, 5000); // Change the quote every 5 seconds (5000 milliseconds)
-});
 
 
 
